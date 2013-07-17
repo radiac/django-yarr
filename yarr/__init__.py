71288c8fe1879d3babcfa00d4827357366906a21
Richard Terry <git@radiac.net>
2013-07-17 16:40:07 +0100

    0.3.4 - cookie-based memory of feed list visibility

--- yarr/__init__.py
+++ yarr/__init__.py
@@ -2,7 +2,10 @@
 Django Yarr - Yet Another RSS Reader
 """
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __license__ = "BSD"
 __author__ = "Richard Terry"
-__credits__ = ["Andrew Rowson"]
+__credits__ = [
+    "Andrew Rowson (growse)",
+    "Aleksandr Pasechnik (russkey)",
+]
