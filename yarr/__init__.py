57808587573bfae3f730953341b83efcadbe32c1
Richard Terry <git@radiac.net>
2013-08-06 10:30:16 +0100

    Bump version to 0.3.7

--- yarr/__init__.py
+++ yarr/__init__.py
@@ -2,10 +2,11 @@
 Django Yarr - Yet Another RSS Reader
 """
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 __license__ = "BSD"
 __author__ = "Richard Terry"
 __credits__ = [
     "Andrew Rowson (growse)",
     "Aleksandr Pasechnik (russkey)",
+    "Spencer Herzberg (sherzberg)",
 ]
