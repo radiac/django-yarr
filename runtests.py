570407aa6aee1b7afd25485870e3d194bd4121c7
Tom Most <twm@freecog.net>
2014-02-16 12:57:16 -0800

    Add Django 1.6 and South to tox.ini
    Add a tox environment which contains Django 1.6, and another which also
    contains South so that migrations at least get superficial testing.
    This requires using the Django management command to run the tests so
    that South's hooks execute.

--- runtests.py
+++ runtests.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python
 
-import os, sys
+import sys
+import os
 
 from django.conf import settings
 
-settings.configure(
-    DEBUG=True,
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-        }
-    },
-    ROOT_URLCONF='yarr.urls',
-    INSTALLED_APPS=(
+
+def configure_django():
+    installed_apps = (
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.admin',
         'yarr',
     )
-)
+    if os.environ.get('USE_SOUTH', ''):
+        installed_apps += ('south',)
 
-from django.test.simple import DjangoTestSuiteRunner
-test_runner = DjangoTestSuiteRunner(verbosity=1)
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        ROOT_URLCONF='yarr.urls',
+        INSTALLED_APPS=installed_apps,
+    )
 
 if __name__ == '__main__':
-    failures = test_runner.run_tests(['yarr', ])
-    if failures:
-        sys.exit(failures)
+    configure_django()
+
+    import django
+    from django.core.management import execute_from_command_line
+
+    # Test discovery changed in Django 1.6 to require a full import path rather
+    # than an app label.
+    if django.VERSION >= (1, 6):
+        args = ['yarr.tests']
+    else:
+        args = ['yarr']
+
+    execute_from_command_line(sys.argv[0:1] + ['test'] + args)
